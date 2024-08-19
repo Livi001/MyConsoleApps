4f9412a3fd31f7fa10090df52d2f2fee1de84927
Livi <Livi@RAVILIVI5>
2024-08-19 09:35:08 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp_001/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Hallo zum  ersten {0}", name);
+            Console.WriteLine($"Hallo zum zweiten {name}");
+
+            int anzahl;
+            Console.WriteLine(" wie viele mal?");
+            string strAnzahl = Console.ReadLine(); 
+            anzahl = int.Parse(strAnzahl);
+
+            for (int i = 0; i < anzahl; i++)
+            {
+                Console.WriteLine($"zum {i}. mal");
+            }
+
+        }
+    }
+}
